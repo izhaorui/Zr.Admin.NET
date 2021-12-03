ea817806367a0c6f1857625f904ee5c772aa9e5c
不做码农 <599854767@qq.com>
2021-12-03 21:59:52 +0800

    新增加 ISysConfigService.cs

---
+++ ZR.Service/System/IService/ISysConfigService.cs
@@ -0,0 +1,17 @@
+using System;
+using ZR.Model.Models;
+using ZR.Model.System;
+
+namespace ZR.Service.System
+{
+    /// <summary>
+    /// 参数配置service接口
+    ///
+    /// @author zhaorui
+    /// @date 2021-09-29
+    /// </summary>
+    public interface ISysConfigService: IBaseService<SysConfig>
+    {
+        SysConfig GetSysConfigByKey(string key);
+    }
+}
