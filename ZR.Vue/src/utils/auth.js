40cab1dd1e0571d713df41f6e918ef77bbdb8f76
samisgod <21gh@163.com>
2021-12-17 16:51:38 +0800

    解决权限较多时Cookie超过最大大小无法登录问题
    暂时使用localStorage替换

--- ZR.Vue/src/utils/auth.js
+++ ZR.Vue/src/utils/auth.js
@@ -4,14 +4,14 @@ import Cookies from 'js-cookie'
 const TokenKey = 'ZR-Token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return localStorage.getItem(TokenKey)
 }
 
 export function setToken(token) {
   // console.log('set token=' + token)
-  return Cookies.set(TokenKey, token)
+  return localStorage.setItem(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return localStorage.removeItem(TokenKey)
 }
