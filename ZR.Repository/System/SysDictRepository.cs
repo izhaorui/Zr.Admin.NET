461e6a4b084cfc151a0c911a29a8528ea9ddc8d1
不做码农 <599854767@qq.com>
2021-12-19 17:31:14 +0800

    修改字典数据倒序排序

--- ZR.Repository/System/SysDictRepository.cs
+++ ZR.Repository/System/SysDictRepository.cs
@@ -22,14 +22,14 @@ namespace ZR.Repository.System
         /// </summary>
         /// <param name="dictType">实体模型</param>
         /// <returns></returns>
-        public PagedInfo<SysDictType> SelectDictTypeList(SysDictType dictType, Model.PagerInfo pager)
+        public PagedInfo<SysDictType> SelectDictTypeList(SysDictType dictType, PagerInfo pager)
         {
             var exp = Expressionable.Create<SysDictType>();
             exp.AndIF(!string.IsNullOrEmpty(dictType.DictName), it => it.DictName.Contains(dictType.DictName));
             exp.AndIF(!string.IsNullOrEmpty(dictType.Status), it => it.Status == dictType.Status);
-            exp.AndIF(!string.IsNullOrEmpty(dictType.DictType), it => it.DictType == dictType.DictType);
+            exp.AndIF(!string.IsNullOrEmpty(dictType.DictType), it => it.DictType.Contains(dictType.DictType));
 
-            return GetPages(exp.ToExpression(), pager);
+            return GetPages(exp.ToExpression(), pager, f => f.DictId, OrderByType.Desc);
         }
 
         /// <summary>
