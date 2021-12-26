ed44d9e52cf949813e71deeaee6ef75a500aeaf3
不做码农 <599854767@qq.com>
2021-12-26 18:28:26 +0800

    新增加roleDept实体

---
+++ ZR.Model/System/SysRoleDept.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZR.Model.System
+{
+    [SqlSugar.SugarTable("sys_role_dept")]
+    [SqlSugar.Tenant(0)]
+    public class SysRoleDept
+    {
+        public long RoleId { get; set; }
+        public long DeptId { get; set; }
+    }
+}
