00b2458f09690cc8d1b4d362a864192294a3627e
不做码农 <599854767@qq.com>
2021-12-27 10:11:22 +0800

    优化代码

--- ZR.Admin.WebApi/Controllers/BaseController.cs
+++ ZR.Admin.WebApi/Controllers/BaseController.cs
@@ -18,7 +18,7 @@ namespace ZR.Admin.WebApi.Controllers
         public static string TIME_FORMAT_FULL = "yyyy-MM-dd HH:mm:ss";
         public static string TIME_FORMAT_FULL_2 = "MM-dd HH:mm:ss";
 
-        protected IActionResult SUCCESS(object data, string timeFormatStr = "MM-dd HH:mm")
+        protected IActionResult SUCCESS(object data, string timeFormatStr = "yyyy-MM-dd HH:mm:ss")
         {
             string jsonStr = GetJsonStr(GetApiResult(data != null ? ResultCode.SUCCESS : ResultCode.FAIL, data), timeFormatStr);
             return Content(jsonStr, "application/json");
@@ -36,13 +36,13 @@ namespace ZR.Admin.WebApi.Controllers
         /// <param name="apiResult"></param>
         /// <param name="timeFormatStr"></param>
         /// <returns></returns>
-        protected IActionResult ToResponse(ApiResult apiResult, string timeFormatStr = "MM-dd HH:mm")
+        protected IActionResult ToResponse(ApiResult apiResult, string timeFormatStr = "yyyy-MM-dd HH:mm:ss")
         {
             string jsonStr = GetJsonStr(apiResult, timeFormatStr);
 
             return Content(jsonStr, "application/json");
         }
-        protected IActionResult ToResponse(long rows, string timeFormatStr = "MM-dd HH:mm")
+        protected IActionResult ToResponse(long rows, string timeFormatStr = "yyyy-MM-dd HH:mm:ss")
         {
             string jsonStr = GetJsonStr(ToJson(rows), timeFormatStr);
 
