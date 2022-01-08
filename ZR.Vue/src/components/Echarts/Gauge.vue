8661aca703171e0b6deed68a2630f713a1950878
不做码农 <599854767@qq.com>
2022-01-08 21:48:30 +0800

    Gauge组件修改

--- ZR.Vue/src/components/Echarts/Gauge.vue
+++ ZR.Vue/src/components/Echarts/Gauge.vue
@@ -3,72 +3,97 @@
 </template>
 
 <script>
-import * as echarts from 'echarts';
+import * as echarts from "echarts";
 
-require('echarts/theme/macarons') // echarts theme
-import { debounce } from '@/utils'
+require("echarts/theme/macarons"); // echarts theme
+import { debounce } from "@/utils";
 
 export default {
   props: {
+    name: {
+      type: String,
+      default: "业务指标",
+    },
+    min: {
+      type: [Object, Number],
+      default: 0,
+    },
+    max: {
+      type: [Object, Number],
+      default: 0,
+    },
+    data: {
+      type: Array,
+      default: () => [
+        {
+          value: "",
+          name: "占比",
+        },
+      ],
+    },
     className: {
       type: String,
-      default: 'chart'
+      default: "chart",
     },
     width: {
       type: String,
-      default: '100%'
+      default: "100%",
     },
     height: {
       type: String,
-      default: '300px'
-    }
+      default: "300px",
+    },
   },
   data() {
     return {
-      chart: null
-    }
+      chart: null,
+    };
   },
   mounted() {
-    this.initChart()
+    this.initChart();
     this.__resizeHandler = debounce(() => {
       if (this.chart) {
-        this.chart.resize()
+        this.chart.resize();
       }
-    }, 100)
-    window.addEventListener('resize', this.__resizeHandler)
+    }, 100);
+    window.addEventListener("resize", this.__resizeHandler);
   },
   beforeDestroy() {
     if (!this.chart) {
-      return
+      return;
     }
-    window.removeEventListener('resize', this.__resizeHandler)
-    this.chart.dispose()
-    this.chart = null
+    window.removeEventListener("resize", this.__resizeHandler);
+    this.chart.dispose();
+    this.chart = null;
   },
   methods: {
     initChart() {
-      this.chart = echarts.init(this.$el, 'macarons')
+      this.chart = echarts.init(this.$el, "macarons");
 
       this.chart.setOption({
         tooltip: {
-          formatter: '{a} <br/>{b} : {c}%'
+          formatter: "{a} <br/>{b} : {c}",
         },
-        toolbox: {
-          feature: {
-            restore: {},
-            saveAsImage: {}
-          }
-        },
+        // toolbox: {
+        //   feature: {
+        //     restore: {},
+        //     saveAsImage: {},
+        //   },
+        // },
         series: [
           {
-            name: '业务指标',
-            type: 'gauge',
-            detail: { formatter: '{value}%' },
-            data: [{ value: 50, name: '完成率' }]
-          }
-        ]
-      })
-    }
-  }
-}
+            name: this.name,
+            type: "gauge",
+            min: this.min,
+            max: this.max,
+            detail: {
+              formatter: "{value}%",
+            },
+            data: this.data,
+          },
+        ],
+      });
+    },
+  },
+};
 </script>
