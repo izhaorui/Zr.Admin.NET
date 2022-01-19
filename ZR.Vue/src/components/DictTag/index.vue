6c8e78d66664d25ab3d8dbef2d9254143385a5bc
不做码农 <599854767@qq.com>
2022-01-19 17:59:31 +0800

    字典新增是否显示value值

--- ZR.Vue/src/components/DictTag/index.vue
+++ ZR.Vue/src/components/DictTag/index.vue
@@ -2,9 +2,11 @@
   <div>
     <template v-for="(item, index) in options">
       <template v-if="values.includes(item.dictValue)">
-        <span v-if="item.listClass == 'default' || item.listClass == ''" :key="item.dictValue" :index="index" :class="item.cssClass">{{ item.dictLabel }}</span>
-        <el-tag size="mini" v-else :disable-transitions="true" :key="item.dictValue" :index="index" :type="item.listClass == 'primary' ? '' : item.listClass" :class="item.cssClass">
-          {{ item.dictLabel }}
+        <span v-if="item.listClass == 'default' || item.listClass == ''" :key="item.dictValue" :index="index" :class="item.cssClass">
+          {{ item.dictLabel }} <i v-if="showValue">#{{item.dictValue}}</i></span>
+        <el-tag size="mini" v-else :disable-transitions="true" :key="item.dictValue" :index="index"
+          :type="item.listClass == 'primary' ? '' : item.listClass" :class="item.cssClass">
+          {{ item.dictLabel }}<i v-if="showValue">#{{item.dictValue}}</i>
         </el-tag>
       </template>
     </template>
@@ -20,6 +22,7 @@ export default {
       default: null,
     },
     value: [Number, String, Array, Boolean],
+    showValue: false,
   },
   computed: {
     values() {
