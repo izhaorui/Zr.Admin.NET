5e3d6a3c8438f767bd46300f68af647ae791a2a6
不做码农 <599854767@qq.com>
2022-03-01 14:28:58 +0800

    add HubsConstant.cs

---
+++ Infrastructure/Constant/HubsConstant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Constant
+{
+    public class HubsConstant
+    {
+        private const string V = "receiveNotice";
+        public static string ReceiveNotice = V;
+        public static string OnlineNum = "onlineNum";
+        public static string MoreNotice = "moreNotice";
+    }
+}
