075bdb8667f65922a5e58e8596569024df595352
不做码农 <599854767@qq.com>
2022-03-09 21:30:59 +0800

    优化signalR连接

--- ZR.Vue/src/utils/signalR.js
+++ ZR.Vue/src/utils/signalR.js
@@ -13,6 +13,8 @@ export default {
   init(url) {
     const connection = new signalR.HubConnectionBuilder()
       .withUrl(url, { accessTokenFactory: () => getToken() })
+      .withAutomaticReconnect()//自动重新连接
+      .configureLogging(signalR.LogLevel.Information)
       .build();
     this.SR = connection;
     // 断线重连
@@ -28,16 +30,19 @@ export default {
     // 启动
     // this.start();
   },
+  /**
+   * 调用 this.signalR.start().then(async () => { await this.SR.invoke("method")})
+   * @returns 
+   */
   async start() {
     var that = this;
 
     try {
       //使用async和await 或 promise的then 和catch 处理来自服务端的异常
-
       await this.SR.start();
-
       //console.assert(this.SR.state === signalR.HubConnectionState.Connected);
       console.log('signalR 连接成功了', this.SR.state);
+      return true;
     } catch (error) {
       that.failNum--;
       console.log(`失败重试剩余次数${that.failNum}`, error)
@@ -46,6 +51,7 @@ export default {
           await this.SR.start()
         }, 5000);
       }
+      return false;
     }
   },
 	// 接收消息处理
