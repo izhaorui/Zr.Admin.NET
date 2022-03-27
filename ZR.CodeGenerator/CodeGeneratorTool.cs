de43e4964e47106735693c8b176882273243a7d0
不做码农 <599854767@qq.com>
2022-03-27 22:05:19 +0800

    新增加vue3代码生成模板

--- ZR.CodeGenerator/CodeGeneratorTool.cs
+++ ZR.CodeGenerator/CodeGeneratorTool.cs
@@ -85,6 +85,7 @@ namespace ZR.CodeGenerator
             GenerateService(replaceDto, dto);
             GenerateControllers(replaceDto, dto);
             GenerateVueViews(replaceDto, dto);
+            GenerateVue3Views(replaceDto, dto);
             GenerateVueJs(replaceDto, dto);
             GenerateSql(replaceDto, dto);
 
@@ -201,7 +202,40 @@ namespace ZR.CodeGenerator
 
             generateDto.GenCodes.Add(new GenCode(6, "index.vue", fullPath, result));
         }
+        /// <summary>
+        /// vue3
+        /// </summary>
+        /// <param name="replaceDto"></param>
+        /// <param name="generateDto"></param>
+        private static void GenerateVue3Views(ReplaceDto replaceDto, GenerateDto generateDto)
+        {
+            string fileName = string.Empty;
+            switch (generateDto.GenTable.TplCategory)
+            {
+                case "tree":
+                    fileName = "TplTreeVue.txt";
+                    break;
+                case "crud":
+                    fileName = "Vue3.txt";
+                    break;
+                case "select":
+                    fileName = "TplVueSelect.txt";
+                    break;
+                default:
+                    break;
+            }
+            fileName = Path.Combine("v3", fileName);
+            var tpl = FileHelper.ReadJtTemplate(fileName);
+            //tpl.Set("vueQueryFormHtml", replaceDto.VueQueryFormHtml);
+            //tpl.Set("VueViewEditFormRuleContent", replaceDto.VueViewEditFormRuleContent);//添加、修改表单验证规则
+            //tpl.Set("VueViewFormContent", replaceDto.VueViewFormHtml);//添加、修改表单
+            //tpl.Set("VueViewListContent", replaceDto.VueViewListHtml);//查询 table列
 
+            var result = tpl.Render();
+            var fullPath = generateDto.IsPreview ? string.Empty : Path.Combine(generateDto.GenCodePath, "ZR.Vue3", "src", "views", generateDto.GenTable.ModuleName.FirstLowerCase(), $"{generateDto.GenTable.BusinessName.FirstUpperCase()}.vue");
+            //Console.WriteLine(result);
+            generateDto.GenCodes.Add(new GenCode(16, "index.vue", fullPath, result));
+        }
         /// <summary>
         /// 生成vue页面api
         /// </summary>
@@ -466,7 +500,7 @@ namespace ZR.CodeGenerator
         /// <param name="replaceDto"></param>
         private static void InitJntTemplate(GenerateDto dto, ReplaceDto replaceDto)
         {
-            //Engine.Current.Clean();
+            Engine.Current.Clean();
             dto.GenTable.Columns = dto.GenTable.Columns.OrderBy(x => x.Sort).ToList();
             bool showCustomInput = dto.GenTable.Columns.Any(f => f.HtmlType.Equals(GenConstants.HTML_CUSTOM_INPUT, StringComparison.OrdinalIgnoreCase));
             //jnt模板引擎全局变量
