193a960c880b073e42f2a70229192a37d05cfbbc
不做码农 <599854767@qq.com>
2022-04-17 21:52:52 +0800

    vue3移至仓库：https://gitee.com/izory/ZRAdmin-vue

--- ZR.Vue3/src/directive/index.js
+++
@@ -1,9 +0,0 @@
-import hasRole from './permission/hasRole'
-import hasPermi from './permission/hasPermi'
-import clipboard from './module/clipboard'
-
-export default function directive(app){
-  app.directive('hasRole', hasRole)
-  app.directive('hasPermi', hasPermi)
-	app.directive('clipboard', clipboard)
-}
