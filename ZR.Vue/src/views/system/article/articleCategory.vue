3df27c52305fc0c516240ffd998dec720ead172c
不做码农 <599854767@qq.com>
2022-11-18 13:40:38 +0800

    add article/articleCategory.vue

---
+++ ZR.Vue/src/views/system/article/articleCategory.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <el-row class="mt10">
+      请移步至 Vue3
+
+      <el-link href="https://gitee.com/izory/ZRAdmin-vue">https://gitee.com/izory/ZRAdmin-vue</el-link>
+    </el-row>
+  </div>
+</template>
