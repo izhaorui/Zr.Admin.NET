723ba3e3136a765c3750ad970c810bf703ed4da6
不做码农 <599854767@qq.com>
2023-02-13 21:41:08 +0800

    新增注解文件生成配置

--- ZR.Admin.WebApi/Extensions/SwaggerExtension.cs
+++ ZR.Admin.WebApi/Extensions/SwaggerExtension.cs
@@ -38,7 +38,7 @@ namespace ZR.Admin.WebApi.Extensions
         public static void AddSwaggerConfig(this IServiceCollection services)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
-            IWebHostEnvironment hostEnvironment = App.GetRequiredService<IWebHostEnvironment>();
+            //IWebHostEnvironment hostEnvironment = App.GetRequiredService<IWebHostEnvironment>();
 
             services.AddSwaggerGen(c =>
             {
@@ -50,7 +50,7 @@ namespace ZR.Admin.WebApi.Extensions
                 });
                 try
                 {
-                    var tempPath = hostEnvironment.ContentRootPath;
+                    var tempPath = "";// hostEnvironment.ContentRootPath;
                     //添加文档注释
                     c.IncludeXmlComments(Path.Combine(tempPath, "ZRAdmin.xml"), true);
                     c.IncludeXmlComments(Path.Combine(tempPath, "ZRModel.xml"), true);
