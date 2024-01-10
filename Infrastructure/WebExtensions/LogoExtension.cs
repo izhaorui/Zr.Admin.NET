35f50ed89c969f2d4d15a9dbd968fffafcc9c51e
不做码农 <599854767@qq.com>
2024-01-10 08:20:03 +0800

    :pencil:修改官网地址

--- Infrastructure/WebExtensions/LogoExtension.cs
+++ Infrastructure/WebExtensions/LogoExtension.cs
@@ -12,14 +12,14 @@ namespace Infrastructure
             Console.ForegroundColor = ConsoleColor.Blue;
             var contentTpl = JnHelper.ReadTemplate("", "logo.txt");
             var content = contentTpl?.Render();
-            var context = App.HttpContext;
+            
             Console.WriteLine(content);
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("🎉源码地址: https://gitee.com/izory/ZrAdminNetCore");
-            Console.WriteLine("📖官方文档：http://www.izhaorui.cn/doc");
-            Console.WriteLine("💰打赏作者：http://www.izhaorui.cn/doc/support.html");
-            Console.WriteLine("📱移动端体验：http://www.izhaorui.cn/h5");
-            Console.WriteLine($"Swagger地址：[后端启动地址]/swagger/index.html");
+            Console.WriteLine("📖官方文档：http://www.izhaorui.cn");
+            Console.WriteLine("💰打赏作者：http://www.izhaorui.cn/member");
+            Console.WriteLine("📱移动端体验：http://demo.izhaorui.cn/h5");
+            Console.WriteLine($"Swagger地址：[你启动的后端地址]/swagger/index.html");
         }
     }
 }
