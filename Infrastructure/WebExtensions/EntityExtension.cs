2052e2d5e5fcc049089813a58880919c3280dfce
不做码农 <599854767@qq.com>
2024-01-11 15:29:42 +0800

    update EntityExtension.cs

--- Infrastructure/WebExtensions/EntityExtension.cs
+++ Infrastructure/WebExtensions/EntityExtension.cs
@@ -18,7 +18,7 @@ namespace Infrastructure
             types.GetProperty("AddTime", flag)?.SetValue(source, DateTime.Now, null);
             types.GetProperty("CreateBy", flag)?.SetValue(source, context.GetName(), null);
             types.GetProperty("Create_by", flag)?.SetValue(source, context.GetName(), null);
-            types.GetProperty("UserId", flag)?.SetValue(source, context.GetUId(), null);
+            //types.GetProperty("UserId", flag)?.SetValue(source, context.GetUId(), null);
             types.GetProperty("DeptId", flag)?.SetValue(source, context.GetDeptId(), null);
 
             return source;
