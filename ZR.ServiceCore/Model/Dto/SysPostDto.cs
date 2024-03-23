303780fcb180e174011371d6ae39b9b99b598170
不做码农 <599854767@qq.com>
2024-03-23 19:27:55 +0800

    add SysPostDto.cs

---
+++ ZR.ServiceCore/Model/Dto/SysPostDto.cs
@@ -0,0 +1,20 @@
+﻿using ZR.Model;
+using ZR.Model.System;
+
+namespace ZR.ServiceCore.Model.Dto
+{
+    public class SysPostDto : SysPost
+    {
+        /// <summary>
+        /// 用户个数
+        /// </summary>
+        public long UserNum { get; set; }
+    }
+
+    public class SysPostQueryDto : PagerInfo
+    {
+        public string PostName { get; set; }
+        public string Status { get; set; }
+        public string PostCode { get; set; }
+    }
+}
