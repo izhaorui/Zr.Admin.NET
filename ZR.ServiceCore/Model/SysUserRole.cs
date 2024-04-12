cde67b998bd776a10f0659c4b6326318f2b40b76
不做码农 <599854767@qq.com>
2024-04-12 12:32:24 +0800

    :zap:优化建表时表名大小写

--- ZR.ServiceCore/Model/SysUserRole.cs
+++ ZR.ServiceCore/Model/SysUserRole.cs
@@ -1,13 +1,11 @@
-﻿using SqlSugar;
-
-namespace ZR.Model.System
+﻿namespace ZR.Model.System
 {
     /// <summary>
     /// 用户角色关联表 用户N-1 角色
     /// </summary>
     [SugarTable("sys_user_role", "用户和角色关联表")]
     [Tenant("0")]
-    public class SysUserRole
+    public class SysUserRole : SysBase
     {
         [SugarColumn(ColumnName = "user_id", IsPrimaryKey = true)]
         public long UserId { get; set; }
