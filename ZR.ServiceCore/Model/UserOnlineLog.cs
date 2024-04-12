373483d6763553af6b1f9ac6ac338a096e25992d
不做码农 <599854767@qq.com>
2024-04-12 12:32:24 +0800

    :zap:优化建表时表名大小写

--- ZR.ServiceCore/Model/UserOnlineLog.cs
+++ ZR.ServiceCore/Model/UserOnlineLog.cs
@@ -4,7 +4,7 @@ namespace ZR.ServiceCore.Model
     /// <summary>
     /// 用户在线时长
     /// </summary>
-    [SugarTable("UserOnlineLog", TableDescription = "用户在线时长")]
+    [SugarTable("userOnlineLog", TableDescription = "用户在线时长")]
     [Tenant("0")]
     public class UserOnlineLog
     {
